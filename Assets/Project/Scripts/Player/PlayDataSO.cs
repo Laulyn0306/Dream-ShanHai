a4fcc5055323143cfd2fa021372a4a3753884e29
Laulyn0306 <759780190@qq.com>
2025-07-04 01:03:39 +0800

    11

---
+++ Assets/Project/Scripts/Player/PlayDataSO.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class PlayDataSO : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
